6eec130682b6cc0493e5f1e581606fee728be0d5
Shyngys Kassymov <chika.kasymov@gmail.com>
2018-01-29 19:11:35 +0600

    Did project setup and added UI logic

--- WordSuggestion/ViewController.m
+++ WordSuggestion/ViewController.m
@@ -8,7 +8,14 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UITextViewDelegate>
+
+@property (nonatomic, weak) IBOutlet UITextView *textView;
+@property (nonatomic, weak) IBOutlet UIActivityIndicatorView *spinner;
+@property (nonatomic, weak) IBOutlet UILabel *suggestedWordsLabel;
+@property (nonatomic, weak) IBOutlet UILabel *lookupTimeLabel;
+
+@property (nonatomic, strong) NSRegularExpression *regex;
 
 @end
 
@@ -16,13 +23,68 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    NSError *error;
+    self.regex = [[NSRegularExpression alloc] initWithPattern:@"\\p{L}[\\p{L}']*(?:-\\p{L}+)*"
+                                                      options:NSRegularExpressionCaseInsensitive
+                                                        error:&error];
+
+    if (error) {
+        NSLog(@"Invalid regex: %@", error.localizedDescription);
+    }
 }
 
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidLoad];
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    [self setStateLoading:true];
+
+    __weak ViewController *weakSelf = self;
+
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        // load data
+
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [weakSelf setStateLoading:false];
+        });
+    });
+}
+
+#pragma mark - Methods
+
+- (void)setStateLoading:(BOOL)loading {
+    self.textView.userInteractionEnabled = !loading;
+    self.textView.alpha = loading ? 0.5 : 1;
+    self.spinner.hidden = !loading;
+
+    if (loading) {
+        [self.spinner startAnimating];
+    } else {
+        [self.spinner stopAnimating];
+    }
+}
+
+#pragma mark - UITextViewDelegate
+
+- (void)textViewDidChange:(UITextView *)textView {
+    NSString *inputText = textView.text;
+    if (!inputText) {
+        return;
+    }
+
+    NSMutableString *textToLookup = @"".mutableCopy;
+
+    NSArray<NSTextCheckingResult *> *results = [self.regex matchesInString:inputText options:NSMatchingReportProgress range:NSMakeRange(0, inputText.length)];
+    int start = MAX(((int)results.count) - 4, 0);
+    for (int i = start; i < results.count; i++) {
+        NSTextCheckingResult *result = results[i];
+
+        NSString *separator = i == start ? @"" : @" ";
+        NSString *parsedText = [inputText substringWithRange:result.range];
+        [textToLookup appendFormat:@"%@%@", separator, parsedText];
+    }
+
+    NSLog(@"Suggest words for input: %@", textToLookup);
 }
 
 
